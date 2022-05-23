273308f2fead2a0bd68642c194c0691c654c274e
Loick <loickleborgne47@hotmail.com>
2022-05-23 11:51:14 +0200

    first commit

---
+++ js/ScrollReveal.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.from_up', { duration: 1000, delay: 250, origin: 'top', distance: '50px' });
+sr.reveal('.from_bottom', { duration: 1000, delay: 250, origin: 'bottom', distance: '50px' });
+sr.reveal('.from_right', { duration: 1000, delay: 250, origin: 'right', distance: '50px' });
+sr.reveal('.from_left', { duration: 1000, delay: 250, origin: 'left', distance: '50px' });
+sr.reveal('.from_center', { duration: 1000, delay: 200, scale: 1.5 });
+sr.reveal('.rotate', { duration: 1000, delay: 250, rotate: { x: 180, z: 180 } });
+
+// === OTHER EXCEPTION === //
+
+
+sr.reveal('.red-left', { duration: 500, delay: 250, rotate: { x: 360, z: 0} });
+sr.reveal('#navbar', { duration: 2000, delay: 250, origin: 'top', distance: '100px' });
+sr.reveal('.anim', { duration: 2000, delay: 250, origin: 'top', distance: '350px' });
+sr.reveal('.anim2', { duration: 2000, delay: 250, origin: 'right', distance: '350px' });
+sr.reveal('.anim3', { duration: 2000, delay: 250, origin: 'right', distance: '350px' });
+sr.reveal('.container-test1-fluid', { duration: 2800, delay: 250, origin: 'left', distance: '550px' });
+sr.reveal('.container-fluid', { duration: 2500, delay: 250, rotate: { x: 80, z: 0} });
+sr.reveal('.container-test2-fluid', { duration: 2800, delay: 250, origin: 'right', distance: '550px' });
+sr.reveal('.apropos', { duration: 2500, delay: 250, origin: 'right', distance: '900px' });
+sr.reveal('.techno-title', { duration: 2500, delay: 250, origin: 'left', distance: '700px' });
+sr.reveal('.icon', { duration: 2500, delay: 250, origin: 'right', distance: '900px' });
+sr.reveal('.skills-fluid', { duration: 2500, delay: 250, origin: 'bottom', distance: '150px' });
+
+
+
+
+
+
+
+
+
